4473e6c22bb6bd9b911c6db7d331a108fc39081b
MichaelAbid <m.abid@rubika-edu.com>
2022-10-11 16:12:06 +0200

    Player Controller V3 ( Player can switch from Pawn )

--- WanderingClouds/Assets/Scripts/PlayerController/PlayerController.cs
+++ WanderingClouds/Assets/Scripts/PlayerController/PlayerController.cs
@@ -1,17 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     private int ControllerNumber;
-    public Pawn pawn;
-
+    public Pawn cPawn;
+    private int pawnIndex = 0;
+    public List<Pawn> listOfAllPawn;
     private void Start()
     {
         ControllerNumber = FindObjectsOfType<PlayerController>().Length;
         ControllerJoin();
+        listOfAllPawn = FindObjectsOfType<Pawn>().ToList();
+        pawnIndex = -1;
+        ChangePawn();
+    }
+
+    public void ChangePawn()
+    {
+        
+
+        pawnIndex = pawnIndex+1<listOfAllPawn.Count ? pawnIndex+1 : 0;
+        int i = 0;
+        foreach (Pawn pawn in listOfAllPawn)
+        {
+            if (!pawn.controlled && i>=pawnIndex)
+            {
+                if (cPawn != null)
+                {
+                    cPawn.controlled = false;
+                }
+                pawn.controlled = true;
+                this.cPawn = pawn;
+                break;
+            }
+            i++;
+        }
     }
 
     public void ControllerJoin()
@@ -22,52 +49,59 @@ public class PlayerController : MonoBehaviour
 
     public void LeftJoyStick(InputAction.CallbackContext callback)
     {
-        if (pawn != null)
+        if (cPawn != null)
         {
-            pawn.MovementInput(callback.ReadValue<Vector2>());
+            cPawn.MovementInput(callback.ReadValue<Vector2>());
         }
     }
 
     public void RightJoyStick(InputAction.CallbackContext callback)
     {
-        if (pawn != null)
+        if (cPawn != null)
         {
-            pawn.CameraMovementInput(callback.ReadValue<Vector2>());
+            cPawn.CameraMovementInput(callback.ReadValue<Vector2>());
         }
     }
 
 
     public void ButtonEst(InputAction.CallbackContext callback)
     {
-        if (pawn != null && callback.performed)
+        if (cPawn != null && callback.performed)
         {
-            pawn.EstButtonInput();
+            cPawn.EstButtonInput();
         }
     }
 
     public void ButtonWest(InputAction.CallbackContext callback)
     {
-        if (pawn != null && callback.performed)
+        if (cPawn != null && callback.performed)
         {
-            pawn.WestButtonInput();
+            cPawn.WestButtonInput();
         }
     }
 
     public void ButtonSouth(InputAction.CallbackContext callback)
     {
-        if (pawn != null && callback.performed)
+        if (cPawn != null && callback.performed)
         {
-            pawn.SouthButtonInput();
+            cPawn.SouthButtonInput();
         }
     }
 
     public void ButtonNorth(InputAction.CallbackContext callback)
     {
-        if (pawn != null && callback.performed)
+        if (cPawn != null && callback.performed)
         {
-            pawn.NorthButtonInput();
+            cPawn.NorthButtonInput();
         }
     }
 
+    public void ButtonSelect(InputAction.CallbackContext callback)
+    {
+        if (callback.performed)
+        {
+            ChangePawn();
+        }
+    }
 
 }
