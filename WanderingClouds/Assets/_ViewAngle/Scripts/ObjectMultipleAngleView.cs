98c04b9fab4db58ed0ea1f60a8ef19fd44ffe320
Arthur DELEYE <a.deleye@rubika-edu.com>
2022-10-17 10:26:41 +0200

    View Angle V2

---
+++ WanderingClouds/Assets/_ViewAngle/Scripts/ObjectMultipleAngleView.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectMultipleAngleView : MonoBehaviour
+{
+    public Transform[] fullObject;
+    public Transform[] keyPosL;
+    public TransformData[] keyPosR;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
