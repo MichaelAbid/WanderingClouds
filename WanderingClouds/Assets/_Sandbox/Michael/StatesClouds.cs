79627b89588f3b9d495af14ebdc39ebd233d7ad2
MichaelAbid <m.abid@rubika-edu.com>
2022-11-14 14:14:51 +0100

    Update StatesClouds.cs

--- WanderingClouds/Assets/_Sandbox/Michael/StatesClouds.cs
+++ WanderingClouds/Assets/_Sandbox/Michael/StatesClouds.cs
@@ -24,6 +24,7 @@ public class StatesClouds : MonoBehaviour
 
     public bool grabed;
     public CLOUD_STATE state = CLOUD_STATE.STATE_BRUME;
+    public BEHAVIOR_STATE bState = BEHAVIOR_STATE.STATE_WANDERING;
 
 
     public float movementSpeed;
@@ -80,33 +81,42 @@ public class StatesClouds : MonoBehaviour
             case CLOUD_STATE.STATE_SOLID:
                 break;
             case CLOUD_STATE.STATE_BRUME:
-                if (shouldMoveToAZone)
+                switch (bState)
                 {
-                    if (Vector3.Distance(positionToGo, transform.position) > 0.2f)
-                    {
-                        Vector3 direction = (positionToGo - transform.position).normalized;
-                        transform.position += direction * movementSpeed * Time.deltaTime;
-
-                    }
-                    else
-                    {
-                        if (!departure)
+                    case BEHAVIOR_STATE.STATE_WANDERING:
+                        if (shouldMoveToAZone)
                         {
-                            if (landing == true)
+                            if (Vector3.Distance(positionToGo, transform.position) > 0.2f)
                             {
-                                shouldMoveToAZone = false;
-                                meshFilter.mesh = zoneToMove.posedMesh;
-                                StartCoroutine(WaitForDeparture(Random.Range(zoneToMove.minTimeBeforeMoving, zoneToMove.maxTimeBeforeMoving)));
+                                Vector3 direction = (positionToGo - transform.position).normalized;
+                                transform.position += direction * movementSpeed * Time.deltaTime;
+
                             }
                             else
                             {
-                                landing = true;
-                                positionToGo.y = positionToGo.y - 1;
+                                if (!departure)
+                                {
+                                    if (landing == true)
+                                    {
+                                        shouldMoveToAZone = false;
+                                        meshFilter.mesh = zoneToMove.posedMesh;
+                                        StartCoroutine(WaitForDeparture(Random.Range(zoneToMove.minTimeBeforeMoving, zoneToMove.maxTimeBeforeMoving)));
+                                    }
+                                    else
+                                    {
+                                        landing = true;
+                                        positionToGo.y = positionToGo.y - 1;
+                                    }
+                                }
+
+
                             }
                         }
-
-                        
-                    }
+                        break;
+                    case BEHAVIOR_STATE.STATE_LAUNCHED:
+                        break;
+                    default:
+                        break;
                 }
                 break;
             default:
@@ -125,8 +135,7 @@ public class StatesClouds : MonoBehaviour
         landing = false;
         shouldMoveToAZone = true;
         departure = true;
-        yield return new WaitForSeconds(0.2f);
-        
+        while(Vector3.Distance(positionToGo, transform.position) > 0.05f) yield return new WaitForEndOfFrame();
         departure = false;
         positionToGo.x = zoneToMove.transform.position.x + Random.Range(-zoneToMove.zoneSize, zoneToMove.zoneSize);
         positionToGo.y = zoneToMove.transform.position.y+1;
@@ -138,11 +147,14 @@ public class StatesClouds : MonoBehaviour
 
     private void OnDrawGizmos()
     {
-        if (shouldMoveToAZone)
+        if (shouldMoveToAZone && bState == BEHAVIOR_STATE.STATE_WANDERING)
         {
             Gizmos.color = Color.yellow;
             Gizmos.DrawLine(transform.position, positionToGo);
         }
     }
+
+
+
 
 }
