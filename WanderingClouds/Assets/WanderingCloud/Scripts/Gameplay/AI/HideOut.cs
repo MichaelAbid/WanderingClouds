368c70eb1dc7d33dfd978a4a2ddd1ef25ce526ed
MichaelAbid <m.abid@rubika-edu.com>
2022-11-21 17:33:19 +0100

    [ADD] Start IA and HideOut

---
+++ WanderingClouds/Assets/WanderingCloud/Scripts/Gameplay/AI/HideOut.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WanderingCloud
+{
+    public class HideOut : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+
+#if UNITY_EDITOR
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = new Color(1,1,0,0.5f);
+            Gizmos.DrawCube(transform.position + (Vector3.up*0.5f), transform.lossyScale);
+        }
+#endif
+    }
+}
