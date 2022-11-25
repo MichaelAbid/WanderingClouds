e03ed92f81055fa076da9beac7acaee8d17937c6
Paul PARET <paret.paul@orange.fr>
2022-11-25 19:17:08 +0100

    [ADD] Started working on aim behavior

--- WanderingClouds/Assets/WanderingCloud/Scripts/Gameplay/Player/PlayerAim.cs
+++ WanderingClouds/Assets/WanderingCloud/Scripts/Gameplay/Player/PlayerAim.cs
@@ -1,12 +1,35 @@
 ﻿using UnityEngine;
+using NaughtyAttributes;
+using Cinemachine;
 
 namespace WanderingCloud.Controller
 {
     public class PlayerAim : MonoBehaviour
     {
-        [HideInInspector] private PlayerBrain player;
-
         [SerializeField] private bool isAiming = false;
 
+        [field: SerializeField, Foldout("References")] public PlayerBrain player;
+
+        //On Payer Aim Input, affect -> Camera, Caracter Speed, Character Animations, Crosshair activation
+
+        //Crosshair and autoaim 
+
+        public void BeginAim()
+        {
+            player.CinemachineAim.m_YAxis = player.CinemachineExplo.m_YAxis;
+            player.CinemachineAim.m_XAxis = player.CinemachineExplo.m_XAxis;
+
+            //Aim VCam takes priority
+            player.CinemachineAim.Priority = player.CinemachineExplo.Priority + 1;
+        }
+
+        public void EndAim()
+        {
+            player.CinemachineExplo.m_YAxis = player.CinemachineAim.m_YAxis;
+            player.CinemachineExplo.m_XAxis = player.CinemachineAim.m_XAxis;
+
+            //Aim VCam takes priority
+            player.CinemachineAim.Priority = player.CinemachineExplo.Priority - 1;
+        }
     }
 }
