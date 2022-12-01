3e24367163e945bdc50b50fbd5d23abf11ee08c1
Manon <manon.ghignoni@gmail.com>
2022-12-01 15:58:36 +0100

    [EDIT] cloud state improvment

--- WanderingClouds/Assets/WanderingCloud/Scripts/Gameplay/LDElements/DestructibleBlocks.cs
+++ WanderingClouds/Assets/WanderingCloud/Scripts/Gameplay/LDElements/DestructibleBlocks.cs
@@ -9,13 +9,7 @@ namespace WanderingCloud.Gameplay
 {
 	public class DestructibleBlocks : MonoBehaviour
 	{
-		private void OnTriggerEnter(Collider collider)
-	{
-		if (collider.GetComponent<CreatureSources>()!= null && collider.GetComponent<CreatureSources>().currentState == CloudState.DESTRUCTOR)
-		{
-			Destroy(this);
-		}
-	}
+
 		
 	}
 }
